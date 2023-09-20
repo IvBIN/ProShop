02fec670ccf9700ad1e5acc15716e407d77bcc51
IvBIN <122294952+IvBIN@users.noreply.github.com>
2023-09-20 20:56:04 +0400

    Add files via upload

---
+++ app/config/db.php
@@ -0,0 +1,17 @@
+<?php
+
+//return [
+//    'provider' => 'pgsql',
+//    'hostname' => 'localhost',
+//    'database' => 'frame',
+//    'username' => 'postgres',
+//    'password' => ''
+//];
+
+return [
+    'provider' => 'pgsql',
+    'hostname' => '194.190.152.134',
+    'database' => 'southdb',
+    'username' => 'leon_ast',
+    'password' => 'tryDB23'
+];
